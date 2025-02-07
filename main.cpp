3c198944f4fb434284fc168a29ad4f000a195421
furutaayumu <k023g0086@g.neec.ac.jp>
2025-02-07 10:51:00 +0900

    fini

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"cirle.h"
+#include"Rectangle.h"
+int main() {
+
+
+	IShape* iShape[2];
+	iShape[0] = new Circle;
+	iShape[1] = new Rectangle;
+
+	for (int i = 0; i < 2; i++) {
+		iShape[i]->Size();
+		iShape[i]->Draw();
+	}
+	for (int i = 0; i < 2; i++) {
+		delete iShape[i];
+	}
+
+	return 0;
+}
